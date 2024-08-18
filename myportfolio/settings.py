119128342139ac6a04eb7bd046cd4f8d6a74dbd5
Veera Deepesh Gondimalla <116485152+deepu3007@users.noreply.github.com>
2024-08-18 14:12:26 +0530

    Update settings.py

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -81,6 +81,24 @@ TEMPLATES = [
     },
 ]
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
+
 WSGI_APPLICATION = 'myportfolio.wsgi.application'
 
 
